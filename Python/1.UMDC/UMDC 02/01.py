b7e65ffd7f8dae8c958869d638dda9d26b033d1b
Mario Vega <45387344+Mariovega99@users.noreply.github.com>
2021-02-13 21:44:37 +0100

    Add files via upload

---
+++ Python/1.UMDC/UMDC 02/01.py
@@ -0,0 +1,25 @@
+"""
+Ejercicio 01
+Escribir dos funciones que permitan calcular:
+    La cantidad de segundos en un tiempo dado en horas, minutos y segundos.
+    La cantidad de horas, minutos y segundos de un tiempo dado en segundos.
+>>> hms_seg(10, 32, 14)
+37934
+>>> seg_hms(37934)
+(10, 32, 14)
+"""
+
+
+def hms_seg(h, m, s):
+    return ((h*60)+m)*60+s
+
+def seg_hms(s):
+    horas = s//3600
+    s = s % 3600
+    minutos = s//60
+    segundos = s % 60
+    return (horas, minutos, segundos)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
